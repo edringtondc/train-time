3cb80ffdad756727b3b7d692d4c6640382775115
Elisabeth Edrington <edringtondc@gmail.com>
2018-11-30 21:40:39 -0700

    finished product

--- assets/javascript/app2.js
+++ assets/javascript/app2.js
@@ -10,54 +10,65 @@ firebase.initializeApp(config);
 
 
 var dataRef = firebase.database();
-var clickCounter = 0
+
 
 $("#submit-button").on("click", function (event) {
     event.preventDefault();
-    clickCounter++;
-    console.log("clickCounter");
+
 
     var trainName = $("#train-name").val().trim();
-    var destination = "france"
+    var destination = $("#train-destination").val().trim();
     var firstTime = $("#first-time").val().trim();
     var tFrequency = $("#train-frequency").val().trim();
 
     dataRef.ref().push({
-        clicks: clickCounter,
         train: trainName,
-        start:  firstTime,
+        start: firstTime,
         where: destination,
         often: tFrequency
     });
 
-    
+    $("#train-name").val("");
+    $("#train-destination").val("");
+    $("#first-time").val("");
+    $("#train-frequency").val("");
+
+    animateTrain();
+
 
 });
 
 dataRef.ref().on("child_added", function (childSnapshot) {
-    console.log("clicks: " + childSnapshot.val().clicks)
-    console.log("train: " + childSnapshot.val().train)
-    console.log("start: " + childSnapshot.val().start)
-    console.log("where: " + childSnapshot.val().where)
-    console.log("often: " + childSnapshot.val().often)
 
     var startConverted = moment(childSnapshot.val().start, "HH:mm").subtract(1, "days");
-
-
     var diffTime = moment().diff(moment(startConverted), "minutes");
     var tFrequency = childSnapshot.val().often;
     var tRemainder = diffTime % tFrequency;
     var minsAway = tFrequency - tRemainder;
 
-
     console.log("diffTime: " + diffTime)
-
-
     var nextArrival = moment().add(minsAway, "minutes").format("hh:mm");
-
 
     $("#table-values").append("<tr><td>" + childSnapshot.val().train + "<td>" + childSnapshot.val().where + "<td>" + childSnapshot.val().often + "<td>" + nextArrival + "<td>" + minsAway);
 
 });
+
+function animateTrain() {
+    console.log("animation running");
+    var train = document.getElementById("train-icon");
+    train.classList.remove("run-animation");
+    void train.offsetWidth;
+    // -> and re-adding the class
+    train.classList.add("run-animation");
+
+    $("#train-icon").animate({
+        left: "+=2000",
+
+    }, 10000, function () { $(this).removeAttr('style'); });    
+    //plays train sound
+    var audio = $("#mysoundclip")[0];
+    audio.play();
+}
+
 
 
